8b994a04aeb594b9d29eda7fb0f4788b820cef50
Jane <106870518+Jane-Mix@users.noreply.github.com>
2024-08-09 14:39:06 +0300

    Add files via upload

---
+++ Dior/js/main.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.swiper__top', {
+  effect: 'fade',
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+const swiperAbout = new Swiper(".slider__about", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 20,
+  freeMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+document.querySelectorAll('.accordeon__triger').forEach((item) => {
+  item.addEventListener('click', () => {
+    item.parentNode.classList.toggle('accordeon__item--active')
+  })
+});
